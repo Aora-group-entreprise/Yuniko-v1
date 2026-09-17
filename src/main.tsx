89af5fb59ec3b0f3c44e2738efb8316744e94d50
Aora-group-entreprise <hushengniaina@gmail.com>
2026-09-17 13:20:00 +0300

    feat(phase10): mount runtime error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FeedPage } from "./features/feed/FeedPage";
+import { AppErrorBoundary } from "./AppErrorBoundary";
 import "./styles.css";
 import "./legacy-layout.css";
 import "./phase4.css";
@@ -23,8 +24,10 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <FeedPage />
-    </QueryClientProvider>
+    <AppErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <FeedPage />
+      </QueryClientProvider>
+    </AppErrorBoundary>
   </React.StrictMode>,
 );
