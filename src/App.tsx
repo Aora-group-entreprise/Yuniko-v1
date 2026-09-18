1cd8171856818adc93b2c57d3d4ff3ea94d4f02d
Aora-group-entreprise <hushengniaina@gmail.com>
2026-09-18 19:06:18 +0300

    refactor(foundation): route app composition through routes layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { FeedPage } from "./features/feed/FeedPage";
 import { AppErrorBoundary } from "./AppErrorBoundary";
+import { AppRoutes } from "./routes";
 import { queryClient } from "./lib/query-client";
 
 export function App() {
   return (
     <AppErrorBoundary>
       <QueryClientProvider client={queryClient}>
-        <FeedPage />
+        <AppRoutes />
       </QueryClientProvider>
     </AppErrorBoundary>
   );
