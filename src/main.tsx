d6a009cba8b39d8924e9ddd72256be3be97db930
Aora-group-entreprise <hushengniaina@gmail.com>
2026-09-18 19:06:03 +0300

    refactor(foundation): move app bootstrap into composition root

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { FeedPage } from "./features/feed/FeedPage";
-import { AppErrorBoundary } from "./AppErrorBoundary";
+import { App } from "./App";
 import "./styles.css";
 import "./legacy-layout.css";
 import "./phase4.css";
@@ -12,23 +10,8 @@ import "./security.css";
 import "./settings.css";
 import "./phase10.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 30_000,
-      gcTime: 5 * 60_000,
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-  },
-});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppErrorBoundary>
-      <QueryClientProvider client={queryClient}>
-        <FeedPage />
-      </QueryClientProvider>
-    </AppErrorBoundary>
+    <App />
   </React.StrictMode>,
 );
